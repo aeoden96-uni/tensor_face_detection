5bbe24029b6e562aa328f3b1b78db22f7baa1d2e
mateo <mateo1559@gmail.com>
2022-05-22 17:52:22 +0200

    wip

--- octave/check_test_set.m
+++ octave/check_test_set.m
@@ -4,7 +4,7 @@ function [] = check_test_set(P,U1)
   resize_to = 80;
 
   fprintf(1, '\nTest start:\n');
-  myDir = "data/testt/";
+  myDir = "data/test/";
   myFiles = dir(myDir);
 
   for k = 3:length(myFiles)
